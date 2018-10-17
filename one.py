47e59209df6e30b6c9c63a8927086c1c13e06911
Amreesh Kumar <amreesh.bkp@gmail.com>
2018-10-17 01:21:01 -0500

    Add files via upload

---
+++ one.py
@@ -0,0 +1,12 @@
+#one.py
+def my_func():
+	print('my_func is beautiful')
+print('Top level of one.py')
+
+
+if __name__ == '__main__':
+    
+     print('one.py is written directly')
+else:
+
+	print('one.py has been imported')
